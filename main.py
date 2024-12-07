8c85feefd464878d1d4eee8a3fb1bd375b24718e
Dahbia Zaidi <orchard@noos.fr>
2024-12-07 23:19:59 +0100

    Modification notion de Shuffle

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from player_mod import Player
 from tournament_mod import Tournament
+import random
 #from match_mod import Match
 #from round_mod import Round
 # Liste des joueurs inscrits pour participer au tournois
+
+
 all_players = [
 Player(name="Gourgues", first_name="Benjamin", date_of_birth="15-12-1952", player_id="AA34567"),
 Player(name="Zidi", first_name="Dahlia", date_of_birth="14- 06-1960", player_id="AA98765"),
@@ -15,7 +18,7 @@ Player(name="Pillet", first_name="Marie-Odile", date_of_birth="20-01-1963", play
 Player(name="Lecorvec", first_name="Danielle", date_of_birth="17-10-1957", player_id="AB27654"),
 Player(name="Fouad", first_name="Nicole", date_of_birth="12-11-1973", player_id="AA87698"),
 Player(name="Fourmond", first_name="Valérie", date_of_birth="07-04-1967", player_id="AZ65373"),
-Player(name="Oussov", first_name="Leo", date_of_birth="03-10-1975", player_id="YUP76584"),
+Player(name="Oussov", first_name="Leo", date_of_birth="03-10-1975", player_id="YU76584"),
 Player(name="Vieille", first_name="Thomas", date_of_birth="03-05-1963", player_id="AA99087"),
 Player(name="Dupont", first_name="Clementine", date_of_birth="13-03-1974", player_id="AC98456"),
 Player(name="Dupontel", first_name="Gilles", date_of_birth="13-03-1975", player_id="AC98459"),
@@ -28,7 +31,7 @@ Player(name="Moreau", first_name="Catherine", date_of_birth="22-09-1990", player
 Player(name="Anton", first_name="Nicole", date_of_birth="25-04-1990", player_id="AN57898"),
 Player(name="Antonin", first_name="Serge", date_of_birth="04-04-1990", player_id="AN57888"),
 Player(name="Antonin", first_name="Matteo", date_of_birth="04-04-1998", player_id="AV57867"),
-Player(name="Vanderloof", first_name="Maia", date_of_birth="04-04-2000", player_id="AV5785"),
+Player(name="Vanderloof", first_name="Maia", date_of_birth="04-04-2000", player_id="AV57855"),
 Player(name="Vita", first_name="Marguerite", date_of_birth="07-02-2000", player_id="AV57858"),
 Player(name="Vacca", first_name="Maya", date_of_birth="07-02-2002", player_id="AM45095"),
 Player(name="Vourc'h", first_name="Madeleine", date_of_birth="07-03-2002", player_id="AM45870"),
@@ -51,21 +54,13 @@ Player(name="Brebion", first_name="Vincent", date_of_birth="13-03-1973", player_
 
 tournament = Tournament("Championnat 2024", location="Paris", date_initial="01/01/2025", date_end="30/01/2025", nb_round=4, description="Premier tournois dans la capitale")
 
+random.shuffle(all_players)
+for player in all_players:
+    print(f"{player.name} {player.first_name} (ID: {player.player_id})")
 
-#all_players.shuffle()
-
-for i in range(0, 8):
+for i in range(0, 43):
     tournament.participant_tournois.append({"Player":all_players[i], "Score":0, "Adversaires":[]})
     
-#for player in tournament.participant_tournois:
-for player in sorted(tournament.participant_tournois, key=lambda x: x["Player"].player_id):
-    print(player["Player"].name + " " + player["Player"].player_id + " Score :" + str(player["Score"]))
-    
-    if len(tournament.participant_tournois) == 0:
-        print("Aucun participant n'a été ajouté au tournoi.")
-        
-        
-        
-    #Trier les participants par leur nom
-    #tournament.participant_tournois.sort(key=lambda player: player.name)
-    
+
+    for player in tournament.participant_tournois:
+        print(player["Player"].name + " " + player["Player"].player_id + " Score :" + str(player["Score"]))
