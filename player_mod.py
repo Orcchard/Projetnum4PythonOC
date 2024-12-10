b40fff1ddac4b91fbd9e40bbc736966a2cd4822b
Dahbia Zaidi <orchard@noos.fr>
2024-12-10 14:47:19 +0100

    maj 10-12

--- player_mod.py
+++ player_mod.py
@@ -1,10 +1,10 @@
 class Player: 
-    def __init__(self, name, first_name, date_of_birth, player_id, score=0):
+    def __init__(self, name, first_name, date_of_birth, player_id):
         self.name = name
         self.first_name = first_name
         self.date_of_birth = date_of_birth
         self.player_id = player_id
-        self.score = score
+        
 
     def __str__(self):
         return f"Nom: {self.first_name}, Prenom: {self.name}, Date de Naissance: {self.date_of_birth}, Score: {self.score} pts"
