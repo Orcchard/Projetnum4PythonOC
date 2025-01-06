229450c4d58ddabeff5e492cc441cabf3bc56f80
Dahbia Zaidi <orchard@noos.fr>
2025-01-06 17:26:19 +0100

    MAJ 06-25 tournament_mod avant sauvegarde json

--- tournament_mod.py
+++ tournament_mod.py
@@ -16,7 +16,7 @@ class Tournament:
         self.nb_round = nb_round
         self.description = description
         self.participant_tournois = []  # Liste pour stocker les 8 joueurs ajoutes au tournois
-        #self.adversaires = [] # quels sont les adversaires que le joueur a rencontré pendant un tournois.
+        self.adversaires = [] # quels sont les adversaires que le joueur a rencontré pendant un tournois.
         self.rounds = [] # 1 round contient 4 matches
         
     def __str__(self):
@@ -28,4 +28,28 @@ class Tournament:
                 f"Debut: {self.date_initial}, Fin: {self.date_end}, "
                 f"Description: {self.description}")
         
+        
+    def tournament_dict(self):
+        return {
+            "name":self.name,
+            "location" :self.location,
+            "date_initial":self.date_initial,
+            "date_end":self.date_end, 
+            "nb_round":self.nb_round,
+            "description":self.description,
+            "participant_tournois":[
+                {"player":participant["Player"].player_id,
+                 "score":participant["Score"],
+                 "adversaires":[adversary.player_id for adversary in participant["Adversaires"]]
+                    
+                }
+                for participant in self.participant_tournois
+            ],
+            "rounds": [round.round_dict() for round in self.rounds],
+            
+                }
+            
+            
+            
+            
         
