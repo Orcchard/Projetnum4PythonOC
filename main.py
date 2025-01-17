415ef79c19b1113c2e1b716392e11e2cc3a79eb2
Dahbia Zaidi <orchard@noos.fr>
2025-01-17 10:40:27 +0100

    MAJ 17-01-25 main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 """Entry point."""
 
 from models.player_mod import Player
-from controllers.controller_principal import Controller
-from views.view_utilisateurs import View
-
-game = Controller()
-
-"""instancie le controller_principal"""
-game.run()
-"""fait executer le programme run du controller_principal"""
+from controllers.controller_principal import ControllerPrincipal
+from views.view_users import View
+def main():
+    game = ControllerPrincipal()
+    
+    """instancie le controller_principal"""
+    game.run()
+    """fait executer le programme run du controller_principal"""
 
 if __name__ == "__main__":
     main()
