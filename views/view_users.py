b5dfb1eec9228d0af6d2619910b622ed8420b89c
Dahbia Zaidi <orchard@noos.fr>
2025-01-22 23:23:43 +0100

    Commit du 22-01-2025 soir

--- views/view_users.py
+++ views/view_users.py
@@ -1,4 +1,15 @@
+import os
+from os import path
+import json
+import sys
 class View:
+    
+    
+    @staticmethod
+    def clear_screen():
+        """Clear the display."""
+        os.system("cls" if sys.platform == "win32" else "clear")
+    
     def main_header(self):
         #Header before the main menu
         #self.clear_screen()
@@ -22,9 +33,26 @@ class View:
         """Récupère le nombre de joueurs"""
         #return len(all_players)
         
-    #@staticmethod
     
-        """Condition dur format de la date de naissance a completer"""
+    
+    @staticmethod
+    def first_prompt():
+        """Proposal to the user to make a choice."""
+        print("\nFaites votre choix et pressez la touche [ENTREE] : ")
+        
+        
+        
+        
+    @staticmethod
+    def new_player_header():
+        """Header to add a New Player'."""
+        View().clear_screen()
+        
+        
+        print("\t**********************************************")
+        print("\t*AJOUT D'UN NOUVEAU JOUEUR")
+        print("\t********************************************\n")
+    
     def prompt_for_player(self):
         """Collect all necessary player data and return as a dictionary."""
         print("Veuillez entrer les informations du joueur :")
@@ -32,14 +60,13 @@ class View:
         first_name = input("Prénom du joueur : ").capitalize()
         player_id = input("\nIdentifiant du joueur (2 Lettres majuscules et 5 nombres) : ").upper()
         birth_date = input("Date de naissance du joueur (format JJ/MM/AAAA) : ")
-
-        # Retourner un dictionnaire contenant toutes les informations
-        return {
+        # Créer un dictionnaire contenant toutes les informations
+        player_input_data = {
             "name": name,
             "first_name": first_name,
             "player_id": player_id,
             "birth_date": birth_date,
         }
+        return player_input_data
         
-    
     
