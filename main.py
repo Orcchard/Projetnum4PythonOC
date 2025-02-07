3b9ca45772348b56d4e4b637103c0a0de366dafb
Dahbia Zaidi <orchard@noos.fr>
2025-02-07 21:51:42 +0100

    Commit main.py 06-02-2025

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 from models.player_mod import Player
 from controllers.controller_principal import ControllerPrincipal
 from views.view_users import View
+
+
 def main():
     game = ControllerPrincipal()
     """instancie le controller_principal"""
     game.run()
     """fait executer le programme run du controller_principal"""
+
 
 if __name__ == "__main__":
     main()
