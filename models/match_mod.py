ed4d013fce4746f94c322a36eb3d9e6f9f71388c
Dahbia Zaidi <orchard@noos.fr>
2025-02-07 21:52:59 +0100

    Commit models 06-02-2025

--- models/match_mod.py
+++ models/match_mod.py
@@ -1,8 +1,3 @@
-import datetime
-import random
-from models.player_mod import Player
-
-
 class Match:
     def __init__(self, player1, player2, player1_score=0, player2_score=0):
         # Initialisation des joueurs et des scores
@@ -10,16 +5,12 @@ class Match:
         self.player2 = player2  # Deuxième joueur
         self.player1_score = player1_score  # Score du premier joueur
         self.player2_score = player2_score  # Score du deuxième joueur
-                # constitution  d'un tuple pour stocker les informations du match
-        #self.match_tuple = ([self.player1, self.player1_score], [self.player2, self.player2_score])
-    
+        # constitution  d'un tuple pour stocker les informations du match
+
     def match_dict(self):
-        return{
-               "player1" : self.player1.player_id,
-               "player2" : self.player2.player_id,
-               "player1_score" : self.player1_score,
-               "player2_score" : self.player2_score,
-               }
-        
-        
-    
+        return {
+            "player1": self.player1.player_id,
+            "player2": self.player2.player_id,
+            "player1_score": self.player1_score,
+            "player2_score": self.player2_score
+            }
--- models/tournament_mod.py
+++ models/tournament_mod.py
@@ -1,11 +1,8 @@
-import random
-from models.player_mod import Player
-from models.match_mod import Match
-from models.round_mod import Round
-
-
 class Tournament:
-    def __init__(self, name, location, date_initial, date_end, nb_round=4, description=""):
+    def __init__(
+        self, name, location, date_initial,
+        date_end, nb_round=4, description=""
+    ):
         """
         Initialise un tournois avec ses details et une liste vide de joueurs.
         """
@@ -15,41 +12,47 @@ class Tournament:
         self.date_end = date_end
         self.nb_round = nb_round
         self.description = description
-        self.participant_tournament = []  # Liste pour stocker les 8 joueurs ajoutes au tournois
-        self.adversaires = [] # quels sont les adversaires que le joueur a rencontré pendant un tournois.
-        self.rounds = [] # 1 round contient 4 matches
-        
+        self.participant_tournament = []
+        self.adversaires = []
+        self.rounds = []
+
     def __str__(self):
-        """
-        Renvoie une representation textuelle du tournois.
-        """
-        
+        """Renvoie une representation textuelle du tournois."""
         return (f"Tournois: {self.name}, Lieu: {self.location}, "
                 f"Debut: {self.date_initial}, Fin: {self.date_end}, "
                 f"Description: {self.description}")
-        
-        
+
     def tournament_dict(self):
         return {
-            "name":self.name,
-            "location" :self.location,
-            "date_initial":self.date_initial,
-            "date_end":self.date_end, 
-            "nb_round":self.nb_round,
-            "description":self.description,
-            "participant_tournament":[
-                {"player":participant["Player"].player_id,
-                 "score":participant["Score"],
-                 "adversaires":[adversary.player_id for adversary in participant["Adversaires"]]
-                    
+            "name": self.name,
+            "location": self.location,
+            "date_initial": self.date_initial,
+            "date_end": self.date_end,
+            "nb_round": self.nb_round,
+            "description": self.description,
+            "participant_tournament": [
+                {
+                    "player": participant["Player"].player_id,
+                    "score": participant["Score"],
+                    "adversaires": [
+                        adversary.player_id
+                        for adversary in participant["Adversaires"]
+                        ],
                 }
                 for participant in self.participant_tournament
             ],
-            "rounds": [round.round_dict() for round in self.rounds],
-            
-                }
-            
-            
-            
-            
-        
+            "rounds": [
+                round.round_dict()
+                for round in self.rounds
+            ],
+        }
+
+    def recreate_tournament(tournament_data,all_players):
+        return Tournament(
+            name=tournament_data["name"],
+            location=tournament_data["location"],
+            date_initial=tournament_data["date_initial"],
+            date_end=tournament_data["date_end"],
+            nb_round=tournament_data["nb_round"],
+            description=tournament_data["description"]
+        )
